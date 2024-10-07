ea32b612d4b78c7c9fe0a9fecad553514c9a621d
David.hs <david.hs@hsress.com>
2024-10-07 15:48:49 +0800

    Revert "feat: do not set target jvm"
    This reverts commit f785e3bce3e2a217d2cf510e265698e27fc1eb38.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,6 +18,10 @@ val key: String = localProperties.getProperty("gpr.key")
 kotlin {
 
     androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
         publishLibraryVariants("release", "debug")
     }
 
