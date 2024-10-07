b1d5e47b3e50939a823ead54e76de0532b0caa6b
David.hs <david.hs@hsress.com>
2024-10-07 16:57:16 +0800

    feat: tasks wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,8 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
+
+tasks.wrapper {
+    gradleVersion = libs.versions.agp.toString()
+    distributionType = Wrapper.DistributionType.ALL
+}
